3c139626cc56396a3579ec699af28081d724ec3b
KirGorodPrivate <mooncity007@ukr.net>
2020-08-30 20:03:58 +0300

    Sidebar link fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,7 +72,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('dashboard') }}" class="brand-link">
       <span class="brand-text font-weight-light">Task Manager</span>
     </a>
 
